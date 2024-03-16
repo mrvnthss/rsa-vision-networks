f0a0c4a85d38da277a60f56821ff83cd56f196f3
Marvin Theiss <mrvnthss@mail.de>
2024-03-16 16:28:57 +0100

    Extend ToDtypeWrapper class

--- src/utils/to_dtype_wrapper.py
+++ src/utils/to_dtype_wrapper.py
@@ -1,21 +1,36 @@
+from typing import Dict, Union
+
 import torch
-from torchvision.transforms import v2
+from torchvision import tv_tensors
+from torchvision.transforms.v2 import Transform, ToDtype
 
 
-class ToDtypeWrapper:
+class ToDtypeWrapper(Transform):
     """
     A wrapper class for torchvision.transforms.v2.ToDtype.
 
-    This class accepts a dtype as a string and a scale parameter. It
-    converts the dtype to the corresponding torch.dtype and passes them
-    to the ToDtype class.
+    This class accepts a dtype as a string or a dictionary and a scale
+    parameter.  If dtype is a string, it is converted to the
+    corresponding torch.dtype.  If dtype is a dictionary, its keys
+    should be from torchvision.tv_tensors and its values should be
+    strings that can be converted to torch.dtype.  The converted
+    dtype(s) and scale parameter are then passed to the ToDtype class.
 
     Args:
-        dtype (str): The dtype to convert to. The string is converted to
-            the corresponding torch.dtype.
+        dtype (Union[str, Dict[str, str]]): The dtype to convert to.
+            If it's a string, it is converted to the corresponding
+            torch.dtype.  If it's a dictionary, its keys should be from
+            torchvision.tv_tensors and its values should be strings that
+            can be converted to torch.dtype.
         scale (bool): Whether to scale the values for images or videos.
+
+    Attributes:
+        to_dtype (ToDtype): An instance of
+            torchvision.transforms.v2.ToDtype.
     """
-    def __init__(self, dtype: str, scale: bool):
+    def __init__(self, dtype: Union[str, Dict[str, str]], scale: bool):
+        super().__init__()
+
         dtype_map = {
             "float32": torch.float32,
             "float": torch.float,
@@ -39,7 +54,30 @@ class ToDtypeWrapper:
             "bool": torch.bool
         }
 
-        self.inner = v2.ToDtype(dtype=dtype_map[dtype], scale=scale)
+        tv_tensor_map = {
+            "Image": tv_tensors.Image,
+            "Video": tv_tensors.Video,
+            "BoundingBoxes": tv_tensors.BoundingBoxes,
+            "Mask": tv_tensors.Mask
+        }
+
+        # Assert that dtype is either a string from dtype_map.keys() or
+        # a dict with keys from tv_tensor_map.keys() and values from dtype_map.keys()
+        if isinstance(dtype, str):
+            assert dtype in dtype_map.keys()
+        elif isinstance(dtype, dict):
+            assert all([k in tv_tensor_map.keys() for k in dtype.keys()])
+            assert all([v in dtype_map.keys() for v in dtype.values()])
+        else:
+            raise ValueError(f"dtype must be a str or dict, got {type(dtype)} instead")
+
+        if isinstance(dtype, str):
+            dtype = dtype_map[dtype]
+        else:  # dict
+            dtype = {tv_tensor_map[k]: dtype_map[v] for k, v in dtype.items()}
+
+        # Call the class's constructor that we are wrapping
+        self.to_dtype = ToDtype(dtype, scale=scale)
 
     def __call__(self, img):
-        return self.inner(img)
+        return self.to_dtype(img)
