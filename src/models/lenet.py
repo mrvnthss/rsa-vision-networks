b1406e09064ad961a38a23a2d6eb157109c12581
Marvin Theiss <mrvnthss@mail.de>
2024-03-27 16:17:13 +0100

    Allow for more flexible image dimensions

--- src/models/lenet.py
+++ src/models/lenet.py
@@ -3,24 +3,26 @@ import torch.nn.functional as F
 
 
 class LeNet(nn.Module):
-    """LeNet-5 architecture proposed by LeCun et al. (1998)."""
-    def __init__(self, num_classes=10):
+    """Modified LeNet-5 architecture (LeCun et al., 1998)."""
+    def __init__(self, img_size=32, num_classes=10):
         super(LeNet, self).__init__()
+        if img_size % 4 != 0:
+            raise ValueError(f"in_size {img_size} is not divisible by 4!")
+        out_size = img_size // 4 - 3
         self.conv1 = nn.Conv2d(1, 6, 5)
         self.conv2 = nn.Conv2d(6, 16, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(16 * 4 * 4, 120)
+        self.fc1 = nn.Linear(16 * out_size ** 2, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, num_classes)
 
     def forward(self, x):
-        # Expected input size: 1x28x28
-        x = F.relu(self.conv1(x))   # 1x28x28 -> 6x24x24
-        x = self.pool(x)            # 6x24x24 -> 6x12x12
-        x = F.relu(self.conv2(x))   # 6x12x12 -> 16x8x8
-        x = self.pool(x)            # 16x8x8 -> 16x4x4
-        x = x.view(-1, 16 * 4 * 4)  # 16x4x4 -> 256
-        x = F.relu(self.fc1(x))     # 256 -> 120
-        x = F.relu(self.fc2(x))     # 120 -> 84
-        x = self.fc3(x)             # 84 -> num_classes
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
