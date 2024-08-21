e2abf85829feb3af8cb743c55fb89e5b58fd7536
Marvin Theiss <mrvnthss@mail.de>
2024-08-21 10:53:31 +0200

    Create constants.py

---
+++ src/utils/constants.py
@@ -0,0 +1,23 @@
+"""Constants used throughout this project."""
+
+
+import numpy as np
+
+
+COLORS = {
+    "red": np.array([165, 30, 55], np.uint8) / 255.0,
+    "gold": np.array([180, 160, 105], np.uint8) / 255.0,
+    "anthracite": np.array([50, 65, 75], np.uint8) / 255.0,
+    "tory-blue": np.array([65, 90, 140], np.uint8) / 255.0,
+    "cerulean": np.array([0, 105, 170], np.uint8) / 255.0,
+    "viking": np.array([80, 170, 200], np.uint8) / 255.0,
+    "bilbao": np.array([50, 110, 30], np.uint8) / 255.0,
+    "chelsea-cucumber": np.array([125, 165, 75], np.uint8) / 255.0,
+    "padua": np.array([130, 185, 160], np.uint8) / 255.0,
+    "thatch": np.array([180, 160, 150], np.uint8) / 255.0,
+    "tapestry": np.array([175, 110, 150], np.uint8) / 255.0,
+    "grenadier": np.array([200, 80, 60], np.uint8) / 255.0,
+    "dark-wood": np.array([145, 105, 70], np.uint8) / 255.0,
+    "tangerine": np.array([210, 150, 0], np.uint8) / 255.0,
+    "tacha": np.array([215, 180, 105], np.uint8) / 255.0
+}
