cbe4472ec4aa3e1c2403f723201da934847fbbff
Marvin Theiss <mrvnthss@mail.de>
2024-09-05 15:36:03 +0200

    Add visualize_colors function in color.py

--- src/visualization/color.py
+++ src/visualization/color.py
@@ -3,11 +3,14 @@
 Functions:
     * get_color(base_color, tint=0, alpha=1): Obtain a modified version
         (tint & alpha) of a base color.
+    * visualize_colors(tint=0, alpha=1): Visualize all (modified) colors
+        next to each other.
 """
 
 
 from typing import Optional
 
+import matplotlib.pyplot as plt
 import numpy as np
 
 from src.utils.constants import COLORS
@@ -57,3 +60,43 @@ def get_color(
         modified_color = np.append(modified_color, alpha)
 
     return modified_color
+
+
+def visualize_colors(
+        tint: Optional[float] = 0,
+        alpha: Optional[float] = 1
+) -> None:
+    """Visualize all (modified) colors next to each other.
+
+    Args:
+        tint: The relative amount of tint to add to each color.
+        alpha: The alpha value of each color for transparency.
+
+    Raises:
+        ValueError: If either the ``tint`` or the ``alpha`` is outside
+          the [0, 1] range.
+    """
+
+    if not 0 <= tint <= 1:
+        raise ValueError(
+            f"'tint' should be between 0 and 1, but got {tint}."
+        )
+
+    if not 0 <= alpha <= 1:
+        raise ValueError(
+            f"'alpha' should be between 0 and 1, but got {alpha}."
+        )
+
+    modified_colors = {
+        color_name: get_color(color_name, tint=tint, alpha=alpha)
+        for color_name in COLORS
+    }
+
+    _, ax = plt.subplots(figsize=(5, 3))
+    for i, (color_name, color_value) in enumerate(modified_colors.items()):
+        ax.add_patch(plt.Rectangle((i, 0), 0.9, 1, color=color_value, label=color_name))
+        ax.text(i + 0.5, -0.02, color_name, ha="center", va="top", rotation=90, fontsize=11)
+    ax.set_xlim(0, len(modified_colors))
+    ax.axis("off")
+    plt.tight_layout()
+    plt.show()
