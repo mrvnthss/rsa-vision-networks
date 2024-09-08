10b602f492a610d5625a802627695dd2f7c1a646
Marvin Theiss <mrvnthss@mail.de>
2024-09-08 21:34:57 +0200

    Add visualize_crop function

--- src/visualization/datasets.py
+++ src/visualization/datasets.py
@@ -5,13 +5,24 @@ Functions:
         into a sprite.
     * get_samples_per_class(data_dir, num_samples_per_class, ...): Grab
         a subset of samples from each class in the dataset.
+    * visualize_crop(img, crop_scale, ...): Visualize a random crop of
+        an image.
 """
 
 
+__all__ = [
+    "create_sprite",
+    "get_samples_per_class",
+    "visualize_crop"
+]
+
 from pathlib import Path
+from typing import Tuple
 
 import numpy as np
-from PIL import Image
+import torch
+from PIL import Image, ImageDraw
+from torchvision.transforms.v2 import Pad, RandomResizedCrop
 
 
 def create_sprite(
@@ -90,9 +101,10 @@ def get_samples_per_class(
 
     # Collect samples from each class
     data_dir = Path(data_dir) / ("train" if train else "test")
-    class_dirs = [d for d in data_dir.iterdir() if d.is_dir()]
     samples = {}
-    for class_dir in class_dirs:
+    for class_dir in data_dir.iterdir():
+        if not class_dir.is_dir():
+            continue
         class_name = class_dir.name
         img_paths = list(class_dir.iterdir())
         selected_indices = rng.choice(len(img_paths), num_samples_per_class, replace=False)
@@ -124,3 +136,73 @@ def get_samples_per_class(
                     ] = samples[class_name][img_idx]
 
     return samples_np
+
+
+def visualize_crop(
+        img: Image.Image,
+        crop_scale: Tuple[float, float],
+        crop_ratio: Tuple[float, float],
+        frame_width: int = 1,
+        frame_color: Tuple[int] = (255, 0, 0),
+        seed: int = 42
+) -> Tuple[Image.Image, Image.Image]:
+    """Visualize a random crop of an image.
+
+    Args:
+        img: The image to crop.
+        crop_scale: The lower and upper bounds for the random area
+          of the crop, before resizing.  The scale is defined with
+          respect to the area of the original image.
+        crop_ratio: The lower and upper bounds for the random aspect
+          ratio of the crop, before resizing.
+        frame_width: The width of the frame that's used to highlight the
+          cropped area in the original image.
+        frame_color: The color of the frame that's used to highlight the
+          cropped area in the original image.
+        seed: The random seed to ensure reproducibility of the random
+          crop.
+
+    Returns:
+        A tuple containing the original image with a frame highlighting
+        the cropped area and the cropped image (resized to the same size
+        as the original image).
+    """
+
+    if img.mode != "RGB":
+        img = img.convert("RGB")
+
+    # Create transform
+    random_resized_crop = RandomResizedCrop(
+        size=img.size,
+        scale=crop_scale,
+        ratio=crop_ratio
+    )
+
+    # Get crop parameters (for visualization purposes)
+    torch.manual_seed(seed)
+    row, col, height, width = random_resized_crop.get_params(
+        img,
+        scale=crop_scale,
+        ratio=crop_ratio
+    )
+
+    # Highlight region to be cropped in original image
+    # NOTE: We draw a bounding box around (!) the region that is to be cropped.  To do so, we pad
+    #       the image to prevent the bounding box from being cut off in case the random crop starts
+    #       or ends at an edge of the image.
+    cropped_img = img.copy()
+    img = Pad(padding=frame_width)(img)
+    crop_box = np.array([col - 1, row - 1, col + height, row + width]) + frame_width
+    draw = ImageDraw.Draw(img)
+    draw.rectangle(
+        crop_box.tolist(),
+        outline=frame_color,
+        width=frame_width
+    )
+
+    # Crop image
+    torch.manual_seed(seed)
+    cropped_img = random_resized_crop(cropped_img)
+    cropped_img = Pad(padding=frame_width, fill=frame_color)(cropped_img)
+
+    return img, cropped_img
